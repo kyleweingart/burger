63485526abffde8d475c3b6a45dda7399b1307ef
Kyle Weingart <kyle.weingart@gmail.com>
2018-05-04 17:16:22 -0400

    updates to routes, orm, controller, css, html, handlebars

--- config/orm.js
+++ config/orm.js
@@ -1,20 +1,78 @@
 var connection = require("./connection.js");
 
+
+function printQuestionMarks(num) {
+    var arr = [];
+  
+    for (var i = 0; i < num; i++) {
+      arr.push("?");
+    }
+  
+    return arr.toString();
+  }
+  
+  // Helper function to convert object key/value pairs to SQL syntax
+  function objToSql(ob) {
+    var arr = [];
+  
+    // loop through the keys and push the key/value as a string int arr
+    for (var key in ob) {
+      var value = ob[key];
+      // check to skip hidden properties
+      if (Object.hasOwnProperty.call(ob, key)) {
+        // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+        if (typeof value === "string" && value.indexOf(" ") >= 0) {
+          value = "'" + value + "'";
+        }
+        // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+        // e.g. {sleepy: true} => ["sleepy=true"]
+        arr.push(key + "=" + value);
+      }
+    }
+  
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
+  }
+  
+  
 // Methods that will execute the necessary MySQL commands in the controllers. 
 // These are the methods that retrieve and store data in the database.
 // ==========================================================================================
 
-// I need to work on these- currently i just copied in code
+
 
 var orm = {
-  selectAll: function(tableInput, callback) {
-    var queryString = "SELECT * FROM ??";
-    connection.query(queryString, [tableInput], function(err, result) {
+
+  selectAll: function(tableInput,callback) {
+    var queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function(err, result) {
       if (err) throw err;
       callback(result);
     });
   },
-  insertOne: function(table, cols, vals, cb) {
+
+
+
+
+//   I'm stuck at this point- I can't seem to get this function working in the burgers_controller file- i might need to simplify this and see if i can get it to work. 
+//   create: function(table, cols, value, callback) {
+ 
+
+//     connection.query(
+//       "INSERT INTO ? SET ?",
+//       {
+//         burger_name: burger_name,
+//       },
+//       function(err, result) {
+//         if (err) throw err;
+        
+
+//         callback(result);
+//       });
+
+//   },
+
+create: function(table, cols, vals, cb) {
     var queryString = "INSERT INTO " + table;
 
     queryString += " (";
@@ -33,23 +91,19 @@ var orm = {
 
       cb(result);
     });
-
   },
-  updateOne: function(table, objColVals, condition, cb) {
-    var queryString = "UPDATE " + table;
 
-    queryString += " SET ";
-    queryString += objToSql(objColVals);
-    queryString += " WHERE ";
-    queryString += condition;
+  
+  
+  updateOne: function(burgerID, callback) {
+    
+   
+    connection.query("UPDATE burgers SET ? WHERE", [{devoured:true}, {id: burgerID}],
+     function(err, result) {
+      if (err) throw err;
+      
 
-    console.log(queryString);
-    connection.query(queryString, function(err, result) {
-      if (err) {
-        throw err;
-      }
-
-      cb(result);
+      callback(result);
     });
   },
 
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -17,32 +17,21 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/burgers", function(req, res) {
-  burger.create(["burger_name"], [req.body.burger_name], function(result) {
+  burger.create(["burger_name"],req.body.burger_name, function(result) {
     // Send back the ID of the new quote
-    res.json({ id: result.insertId });
+    // res.json({ id: result.insertId });
+    res.json({id: result.insertID});
   });
 });
 
-router.put("/api/burgers/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
+router.put("/burger/eat/:id", function(req, res) {
+  
 
-  console.log("condition", condition);
-
-  burger.update(
-    {
-      sleepy: req.body.sleepy
-    },
-    condition,
-    function(result) {
-      if (result.changedRows === 0) {
-        // If no rows were changed, then the ID must not exist, so 404
-        return res.status(404).end();
-      }
-      res.status(200).end();
-
-    }
-  );
+  burger.updateOne(req.params.id,function(){
+      res.redirect('/index');
+  });
+   
 });
 
-// Export routes for server.js to use.
+
 module.exports = router;
